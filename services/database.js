4d69e27474880f5f9a8d43c86cc95fa71e7714b6
rebekkabb <breedis.rebekka@gmail.com>
2019-07-11 22:32:30 +0300

    Fixed database environment variables

--- services/database.js
+++ services/database.js
@@ -1,18 +1,13 @@
 const mongo = require('mongodb').MongoClient;
 const mongo_url = process.env.MONGO_URL;
-const mongo_user = process.env.MONGO_USER;
-const mongo_password = process.env.MONGO_PASSWORD;
 let db = null;
 
 exports.connectDatabase = (callback) => {
     mongo.connect(mongo_url, {
         useNewUrlParser: true,
-        auth: {
-            user: mongo_user,
-            password: mongo_password
-        }
     }, (err, client) => {
-        db = client.db('fridgely');
+        if (err) throw err;
+        db = client.db();
         callback();
     });
 };
