e88fd138d66b47d7ac1d1ffde104b50a731271b9
rebekkabb <breedis.rebekka@gmail.com>
2019-07-11 22:01:36 +0300

    Added database env variables

--- services/database.js
+++ services/database.js
@@ -1,9 +1,17 @@
 const mongo = require('mongodb').MongoClient;
-const url = "mongodb://localhost:27017/fridgely";
+const mongo_url = process.env.MONGO_URL;
+const mongo_user = process.env.MONGO_USER;
+const mongo_password = process.env.MONGO_PASSWORD;
 let db = null;
 
 exports.connectDatabase = (callback) => {
-    mongo.connect(url, { useNewUrlParser: true }, (err, client) => {
+    mongo.connect(mongo_url, {
+        useNewUrlParser: true,
+        auth: {
+            user: mongo_user,
+            password: mongo_password
+        }
+    }, (err, client) => {
         db = client.db('fridgely');
         callback();
     });
