595f2e2b9521bd9b3d929ec658bc5dc2c1375e68
rebekkabb <breedis.rebekka@gmail.com>
2019-07-14 04:01:30 +0300

    More error page mods

--- app.js
+++ app.js
@@ -72,6 +72,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
+    res.locals.status = err.status;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
